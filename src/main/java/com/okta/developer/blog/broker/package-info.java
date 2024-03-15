23ee4768a4b88aa0fcbb3fe6c8fac20c117027d5
Omar Ben sghaier <omarbensghaier@MacBook-Air-de-Omar.local>
2024-03-15 21:14:39 +0100

    Initial version of blog generated by generator-jhipster@8.1.0

---
+++ src/main/java/com/okta/developer/blog/broker/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring cloud consumers and providers
+ */
+package com.okta.developer.blog.broker;
---
+++ src/test/java/com/okta/developer/blog/IntegrationTest.java
@@ -0,0 +1,23 @@
+package com.okta.developer.blog;
+
+import com.okta.developer.blog.config.AsyncSyncConfiguration;
+import com.okta.developer.blog.config.EmbeddedKafka;
+import com.okta.developer.blog.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BlogApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@EmbeddedKafka
+public @interface IntegrationTest {
+}
